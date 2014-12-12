7b194f44ec4bc6ffd826c8e24c2aa348e5dfe560
Henk Reder <henk@anestuary.com>
2014-12-12 11:42:12 -0500

    whole bunch of styling changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ActiveRecord::Base
 
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
   has_many :inverse_friends, through: :inverse_friendships, source: :user
+
   #other stuff here
 end
